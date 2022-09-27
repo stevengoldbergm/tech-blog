3d1708217a668965ae11f875f1fcb6619650256f
SG_Demi <stevengoldbergm@gmail.com>
2022-09-26 22:21:44 -0500

    Added public folder to code structure and added login script to handle user login/creation

--- models/Post.js
+++ models/Post.js
@@ -11,21 +11,17 @@ Post.init(
       primaryKey: true,
       autoIncrement: true,
     },
-    name: {
+    title: {
       type: DataTypes.STRING,
       allowNull: false,
     },
-    description: {
+    text: {
       type: DataTypes.STRING,
     },
     date_created: {
       type: DataTypes.DATE,
       allowNull: false,
       defaultValue: DataTypes.NOW,
-    },
-    needed_funding: {
-      type: DataTypes.FLOAT,
-      allowNull: false,
     },
     user_id: {
       type: DataTypes.INTEGER,

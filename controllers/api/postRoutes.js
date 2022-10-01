97bfcfcad77bf2b9677aa570fb8643801a8d095d
SG_Demi <stevengoldbergm@gmail.com>
2022-10-01 18:13:36 -0500

    Trying to figure out why I can't run back-end server calls unless it's from the front end with fetch. . .

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -3,6 +3,7 @@ const { Post } = require('../../models');
 const withAuth = require('../../utils/auth');
 const helpers = require('../../utils/helpers');
 
+// Create Post
 router.post('/', withAuth, async (req, res) => {
   try {
     const newPost = await Post.create({
@@ -16,6 +17,49 @@ router.post('/', withAuth, async (req, res) => {
   }
 });
 
+// Get Single Post
+router.put('/:id',  withAuth, async (req, res) => {
+  try {
+    const postData = await Post.findOne({
+      where: {
+        id: req.params.id,
+        user_id: req.session.user_id,
+      },
+    });
+
+    if (!postData) {
+      res.status(404).json({ message: 'No post found with this id!' });
+      return;
+    }
+
+    res.status(200).json(postData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// Update Post
+router.put('/:id', withAuth, async (req, res) => {
+  try {
+    const postData = await Post.update(req.body, {
+      where: {
+        id: req.params.id,
+        user_id: req.session.user_id,
+      },
+    });
+
+    if (!postData) {
+      res.status(404).json({ message: 'No post found with this id!' });
+      return;
+    }
+
+    res.status(200).json(postData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// Delete Post
 router.delete('/:id', withAuth, async (req, res) => {
   try {
     const postData = await Post.destroy({
--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -18,7 +18,7 @@ router.get('/', async (req, res) => {
 
     // Serialize data so the template can read it
     const posts = postData.map((post) => post.get({ plain: true }));
-    // console.log(posts); // working
+    console.log(posts); // working
     // console.log('logged in?',req.session.logged_in) // working
     
     res.render('main', { 
